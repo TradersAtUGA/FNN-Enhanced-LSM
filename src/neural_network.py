9d979abb918bb236d121781c545d667a5e5db5da
Brennan Davenport <brennandavenport9@gmail.com>
2025-05-20 15:31:49 -0500

    fixed file struct

--- src/neural_network.py
+++ src/neural_network.py
@@ -2,4 +2,18 @@ import torch
 import torch.nn as nn
 
 # Feedforward Neural Network (FNN), also known as a Multilayer Perceptron (MLP)
-class FNN
+class LSMContinuationNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(2, 32),
+            nn.LeakyReLU(0.01),
+            nn.Linear(32, 16),
+            nn.LeakyReLU(0.01),
+            nn.Linear(16, 8),
+            nn.LeakyReLU(0.01),
+            nn.Linear(8, 1)
+        )
+    
+    def forward(self, x):
+        return self.model(x)
